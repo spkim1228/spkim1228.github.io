ca5e659dca3e92967cc5228a5b50ab5cac97424c
Paul Kim <spkim@protonmail.com>
2026-09-09 23:34:58 -0400

    github pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+//import mdx from '@astrojs/mdx';
+//import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://spkim1228.github.io',
+  //integrations: [mdx(), sitemap()],
+});
