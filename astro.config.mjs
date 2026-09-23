5a000053f961c35f9b86078f339c43e811ab77c3
Paul Kim <spkim@protonmail.com>
2026-09-23 02:10:30 -0400

    css reset

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-//import mdx from '@astrojs/mdx';
-//import sitemap from '@astrojs/sitemap';
+import { defineConfig , fontProviders} from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://spkim1228.github.io',
-  //integrations: [mdx(), sitemap()],
+  integrations: [sitemap()],
+  fonts: [{
+    provider: fontProviders.google(),
+    name: 'Atkinson Hyperlegible Next',
+    cssVariable: '--font-atkinson',
+    weights: ['200 800'], 
+    styles: ['normal'],
+    fallbacks: ['system-ui', 'sans-serif'],
+  }],
 });
